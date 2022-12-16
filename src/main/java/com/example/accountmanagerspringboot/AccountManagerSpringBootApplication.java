7647f4227fcdbfeed0ec22fdfa1acbfe4c3e98f0
Daniil Zemnov <zemnowf@gmail.com>
2022-12-16 15:27:10 +0300

    first commit

---
+++ src/main/java/com/example/accountmanagerspringboot/AccountManagerSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.accountmanagerspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManagerSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountManagerSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/accountmanagerspringboot/AccountManagerSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accountmanagerspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagerSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
